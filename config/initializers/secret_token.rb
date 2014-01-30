782ad85e23489641504c1f06d1a99f160703170c
rajeshthummalapally <rajesh.thummalapally@gmail.com>
2014-01-30 09:35:23 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CustomPageStyling::Application.config.secret_key_base = '08c2d2be3f65e88cda1c9aaaea0e2e731bc768b8c8a4e856a37310e99c841b1abd892a9dc0a2c415c9328f5b28f8a622787851b64a9de1954d5877665a17cd4e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CustomPageStyling::Application.config.session_store :cookie_store, key: '_custom_page_styling_session'
